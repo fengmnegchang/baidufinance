54220f477c7106dcb2f9fe362d0f60b09615a8e0
fengmnegchang <624926379@qq.com>
2017-10-11 17:06:25 +0800

    引导页

---
+++ baidufinance/src/com/open/baidu/finance/activity/CommonDotPagerFragmentFragmentActivity.java
@@ -0,0 +1,68 @@
+/**
+ *****************************************************************************************************************************************************************************
+ * 
+ * @author :fengguangjing
+ * @createTime:2017-10-11下午3:07:35
+ * @version:4.2.4
+ * @modifyTime:
+ * @modifyAuthor:
+ * @description:
+ *****************************************************************************************************************************************************************************
+ */
+package com.open.baidu.finance.activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+
+import com.open.android.activity.common.CommonCommonFragmentActivity;
+import com.open.baidu.finance.R;
+import com.open.baidu.finance.fragment.CommonDotFragmentPagerFragment;
+
+/**
+ *****************************************************************************************************************************************************************************
+ * 
+ * @author :fengguangjing
+ * @createTime:2017-10-11下午3:07:35
+ * @version:4.2.4
+ * @modifyTime:
+ * @modifyAuthor:
+ * @description:
+ *****************************************************************************************************************************************************************************
+ */
+public class CommonDotPagerFragmentFragmentActivity extends CommonCommonFragmentActivity{
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.open.enrz.activity.CommonFragmentActivity#initValue()
+	 */
+	@Override
+	protected void initValue() {
+		// TODO Auto-generated method stub
+		if (getIntent().getStringExtra("URL") != null) {
+			url = getIntent().getStringExtra("URL");
+		} else {
+		}
+		addfragment();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.open.android.activity.CommonFragmentActivity#addfragment()
+	 */
+	@Override
+	public void addfragment() {
+		// TODO Auto-generated method stub
+		super.addfragment();
+		Fragment fragment = CommonDotFragmentPagerFragment.newInstance(url, true);
+		getSupportFragmentManager().beginTransaction().replace(R.id.id_common_fragment, fragment).commit();
+	}
+
+	public static void startCommonDotPagerFragmentFragmentActivity(Context context, String url) {
+		Intent intent = new Intent();
+		intent.putExtra("URL", url);
+		intent.setClass(context, CommonDotPagerFragmentFragmentActivity.class);
+		context.startActivity(intent);
+	}
+}
---
+++ baidufinance/src/com/open/baidu/finance/fragment/StartFragment1.java
@@ -0,0 +1,53 @@
+/**
+ *****************************************************************************************************************************************************************************
+ * 
+ * @author :fengguangjing
+ * @createTime:2017-10-11下午3:48:03
+ * @version:4.2.4
+ * @modifyTime:
+ * @modifyAuthor:
+ * @description:
+ *****************************************************************************************************************************************************************************
+ */
+package com.open.baidu.finance.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.open.android.fragment.BaseV4Fragment;
+import com.open.android.fragment.CommonV4Fragment;
+import com.open.android.json.CommonJson;
+import com.open.baidu.finance.R;
+
+/**
+ ***************************************************************************************************************************************************************************** 
+ * 
+ * @author :fengguangjing
+ * @createTime:2017-10-11下午3:48:03
+ * @version:4.2.4
+ * @modifyTime:
+ * @modifyAuthor:
+ * @description:
+ ***************************************************************************************************************************************************************************** 
+ */
+public class StartFragment1 extends BaseV4Fragment<CommonJson, StartFragment1> {
+	public int layoutId;
+	public static StartFragment1 newInstance(String url, boolean isVisibleToUser,int layoutId) {
+		StartFragment1 fragment = new StartFragment1();
+		fragment.setFragment(fragment);
+		fragment.isVisibleToUser = isVisibleToUser;
+		fragment.layoutId = layoutId;
+		return fragment;
+	}
+
+	@Nullable
+	@Override
+	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		View view = inflater.inflate(layoutId, container, false);
+		return view;
+	}
+
+}
